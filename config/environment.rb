b04d79b49d6682bf8039fba019a2d47a5ba68746
Rafael Bento <rafaelhgsbento@gmail.com>
2020-08-25 14:12:53 -0300

    Configurando banco de dados MySQL
    - Criado o arquivo .env.yml local para definir as variáveis de ambiente
    com os dados de acesso ao banco de dados;
    - .env.yml adicionado ao gitignore para que fique somente local;
    - .env.yml.sample como exemplo para quem precisar configurar localmente
    suas próprias variáveis;
    - Alteração no arquivo environment para ler o arquivo .env.yml.
    
    Tive bastante problema com o mysql não sei por algum conflito com o
    Postgres na minha máquina ou se por causa da configuração que usava para
    meus projetos em Node.js. Foi corrigido agora e vou detalhar no
    README.md o que deve ser feito para o caso de encontrar algum problema.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Load Enviroment Variables
+env_file = File.join(Rails.root, '.env.yml')
+
+if File.exists?(env_file)
+  env_variables = YAML.load(File.open(env_file))[Rails.env]
+
+  if env_variables.present?
+    env_variables.each do |key, value|
+      ENV[key.to_s] = value.to_s
+    end
+  end
+end
